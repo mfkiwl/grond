d64bf5dc9c9d73b520cfb9838609d7d47117e534
mmetz <mmetz@gfz-potsdam.de>
2020-10-26 16:22:48 +0100

    dyn rupt report: Refined slip map and general report behaviour

--- src/problems/dynamic_rupture/plot.py
+++ src/problems/dynamic_rupture/plot.py
@@ -40,109 +40,120 @@ class DynamicRuptureSlipMap(PlotConfig):
             section='solution',
             feather_icon='grid',  # alternatively: wind
             description=u'''
-Slip distribution and rake of the finite slip solution. The absolute slip
-is color coded. The black star marks the nucleation point.
-Contour lines indicate the rupture evolution in %.1f s intervals.
+Slip distribution and rake of the pseudo dynamic rupture model. The absolute
+slip is color coded. The arrows are scaled based on the in plane slip. The
+black star marks the nucleation point. Contour lines indicate the rupture
+evolution in %.1f s intervals.
 ''' % self.dt_contour)
 
     def draw_figures(self, history, problem):
-        source = history.get_best_source()
+        source_best = history.get_best_source()
+        source_mean = history.get_mean_source()
 
         store_ids = problem.get_gf_store_ids()
         store = problem.get_gf_store(store_ids[0])
 
         interpolation = 'nearest_neighbor'
 
-        source.discretize_patches(store, interpolation)
-        patches = source.patches
-        dislocations = source.get_okada_slip(scale_slip=True)
+        sources = (source_best, source_mean)
+        label = ('Ensemble best', 'Ensemble mean')
 
-        patches_x = num.array([p.ix for p in patches])\
-            .reshape(source.nx, source.ny)
-        patches_y = num.array([p.iy for p in patches])\
-            .reshape(source.nx, source.ny)
-        patches_t = num.array([p.time for p in patches])\
-            .reshape(source.nx, source.ny)
+        for i, (source, label) in enumerate(zip(sources, label)):
+            fig, ax = plt.subplots(1, 1)
 
-        fig = plt.figure()
-        ax = fig.gca()
+            # ToDo in function with "mean", "best" as arg
+            source.discretize_patches(store, interpolation)
+            patches = source.patches
+            dislocations = source.get_okada_slip(scale_slip=True)
 
-        abs_disloc = num.linalg.norm(dislocations, axis=1)
-        abs_disloc = abs_disloc.reshape(source.nx, source.ny)
+            patches_x = num.array([p.ix for p in patches])\
+                .reshape(source.nx, source.ny)
+            patches_y = num.array([p.iy for p in patches])\
+                .reshape(source.nx, source.ny)
+            patches_t = num.array([p.time for p in patches])\
+                .reshape(source.nx, source.ny)
 
-        im = ax.imshow(
-            abs_disloc.T,
-            cmap='YlOrRd',
-            origin='upper',
-            aspect='equal',
-            extent=(0., source.length, 0., source.width))
+            abs_disloc = num.linalg.norm(dislocations, axis=1)
+            abs_disloc = abs_disloc.reshape(source.nx, source.ny)
 
-        patches_t -= patches_t.min()
+            im = ax.imshow(
+                abs_disloc.T,
+                cmap='YlOrRd',
+                origin='upper',
+                aspect='equal',
+                extent=(0., source.length, 0., source.width))
 
-        nlevels = patches_t.max() // self.dt_contour
-        contours = num.arange(nlevels) * self.dt_contour
-        contours += patches_t.min()
+            patches_t -= patches_t.min()
 
-        def truncate_colormap(cmap, minval=0.0, maxval=1.0, n=100):
-            return colors.LinearSegmentedColormap.from_list(
-                'trunc({n},{a:.2f},{b:.2f})'.format(
-                    n=cmap.name, a=minval, b=maxval),
-                cmap(num.linspace(minval, maxval, n)))
+            nlevels = patches_t.max() // self.dt_contour
+            contours = num.arange(nlevels) * self.dt_contour
+            contours += patches_t.min()
 
-        cmap = truncate_colormap(plt.get_cmap('winter'), 0., 0.8)
+            def truncate_colormap(cmap, minval=0.0, maxval=1.0, n=100):
+                return colors.LinearSegmentedColormap.from_list(
+                    'trunc({n},{a:.2f},{b:.2f})'.format(
+                        n=cmap.name, a=minval, b=maxval),
+                    cmap(num.linspace(minval, maxval, n)))
 
-        contour = ax.contour(
-            patches_x + source.length/2, patches_y, patches_t,
-            levels=contours, alpha=.8, colors='k')
+            cmap = truncate_colormap(plt.get_cmap('winter'), 0., 0.8)
 
-        labels = ax.clabel(
-            contour, contour.levels[::2],
-            inline=True, fmt='%.1f s')
+            contour = ax.contour(
+                patches_x + source.length/2, patches_y, patches_t,
+                levels=contours, alpha=.8, colors='k')
 
-        for l in labels:
-            l.set_rotation(0.)
-            l.set_fontweight('semibold')
-            l.set_fontsize('small')
-            l.set_path_effects([
-                patheffects.Stroke(linewidth=1.25, foreground='beige'),
-                patheffects.Normal()])
+            labels = ax.clabel(
+                contour, contour.levels[::2],
+                inline=True, fmt='%.1f s')
 
-        slip_strike = dislocations[:, 0]
-        slip_dip = dislocations[:, 1]
+            for l in labels:
+                l.set_rotation(0.)
+                l.set_fontweight('semibold')
+                l.set_fontsize('small')
+                l.set_path_effects([
+                    patheffects.Stroke(linewidth=1.25, foreground='beige'),
+                    patheffects.Normal()])
 
-        patch_length = source.length / source.nx
-        patch_width = source.width / source.ny
+            slip_strike = dislocations[:, 0]
+            slip_dip = dislocations[:, 1]
 
-        x_quiver = num.repeat(num.arange(source.nx), source.ny)\
-            * patch_length + patch_length/2.
-        y_quiver = num.tile(num.arange(source.ny), source.nx)\
-            * patch_width + patch_width/2.
+            patch_length = source.length / source.nx
+            patch_width = source.width / source.ny
 
-        ax.quiver(
-            x_quiver, y_quiver, slip_strike, -slip_dip,
-            facecolor='none', edgecolor='k', linewidth=.7,
-            scale=75 / abs_disloc.max(), headwidth=3,
-            cmap='YlOrRd', alpha=.6)
+            x_quiver = num.repeat(num.arange(source.nx), source.ny)\
+                * patch_length + patch_length/2.
+            y_quiver = num.tile(num.arange(source.ny), source.nx)\
+                * patch_width + patch_width/2.
 
-        ax.invert_yaxis()
+            ax.quiver(
+                x_quiver, y_quiver, slip_strike, slip_dip,
+                facecolor='none', edgecolor='k', linewidth=.7,
+                scale=75 / abs_disloc.max(), headwidth=3,
+                cmap='YlOrRd', alpha=.6)
 
-        nucleation_x = ((source.nucleation_x + 1.) / 2.) * source.length
-        nucleation_y = ((source.nucleation_y + 1.) / 2.) * source.width
+            ax.invert_yaxis()
 
-        ax.scatter(
-            nucleation_x, nucleation_y,
-            s=60, color='w', edgecolor='k', marker='*',
-            linewidths=.5, alpha=.7)
+            nucleation_x = ((source.nucleation_x + 1.) / 2.) * source.length
+            nucleation_y = ((source.nucleation_y + 1.) / 2.) * source.width
 
-        ax.xaxis.set_major_formatter(FuncFormatter(km_fmt))
-        ax.yaxis.set_major_formatter(FuncFormatter(km_fmt))
+            ax.scatter(
+                nucleation_x, nucleation_y,
+                s=60, color='w', edgecolor='k', marker='*',
+                linewidths=.5, alpha=.7)
 
-        ax.set_xlabel('Length [km]')
-        ax.set_ylabel('Width [km]')
+            ax.xaxis.set_major_formatter(FuncFormatter(km_fmt))
+            ax.yaxis.set_major_formatter(FuncFormatter(km_fmt))
 
-        cmap = fig.colorbar(
-            im, orientation='horizontal', pad=0.2, shrink=.8,
-            format='%.2f')
-        cmap.set_label('Slip [m]')
+            ax.set_xlabel('Length [km]')
+            ax.set_ylabel('Width [km]')
 
-        yield PlotItem(name='fig_1'), fig
+            cmap = fig.colorbar(
+                im, orientation='horizontal', pad=0.2, shrink=.8,
+                ax=ax, format='%.2f')
+            cmap.set_label('Slip [m]')
+
+            item = PlotItem(
+                name=label.replace(' ', '_').lower(),
+                title='%s slip distribution' % (label.lower()),
+                description=u'')
+
+            yield item, fig
